31328cdd3ce77576a2d4a1bc23227cf04702a583
Alok <alokgithub@gmail.com>
2024-05-14 22:11:14 +0800

    Joblib test

--- experiments/temp.py
+++ experiments/temp.py
@@ -37,11 +37,12 @@ def wait():
         pass
 
 if __name__ == "__main__":
+    start = time.time()
     try:
         parallel = Parallel(n_jobs=5, timeout=3, require='sharedmem')
         parallel(delayed(wait)() for i in range(5))
     except multiprocessing.context.TimeoutError:
-        print("Task failed successfully")
+        print("Task failed successfully", time.time() - start)
         pass
     # runtimes = []
     # architecture = {'nodes': [{'type': 'Input', 'params': {'input_dim': 18}}, {'type': 'IPReservoir', 'params': {'units': 1516, 'lr': 0.3794160763282813, 'sr': 0.9412495273142261, 'mu': 0.09315428782964497, 'sigma': 1.9996893018324113, 'learning_rate': 0.007115438623711385, 'input_connectivity': 0.19270834464023434, 'rc_connectivity': 0.4139009070019663, 'fb_connectivity': 0.4407973219740993}}, {'type': 'LMS', 'params': {'output_dim': 18, 'alpha': 0.3507005013535671}}, {'type': 'NVAR', 'params': {'delay': 5, 'order': 2, 'strides': 3}}, {'type': 'Ridge', 'params': {'output_dim': 18, 'ridge': 6.563310358305086e-06}}, {'type': 'IPReservoir', 'params': {'units': 68, 'lr': 0.8788496628894418, 'sr': 0.660379110102254, 'mu': 0.16625820180299722, 'sigma': 0.5806266716741275, 'learning_rate': 0.008367985016205044, 'input_connectivity': 0.16643347500247355, 'rc_connectivity': 0.136383107016403, 'fb_connectivity': 0.11601073175108537}}, {'type': 'Ridge', 'params': {'output_dim': 18, 'ridge': 2.2092030120388196e-05}}], 'edges': [[0, 1], [1, 2], [2, 3], [1, 4], [2, 5], [3, 6], [4, 6], [5, 6]]}
