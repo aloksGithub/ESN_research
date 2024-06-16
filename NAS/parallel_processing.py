a7a853596b45b9354b89c10f1488502567e2a214
Alok <alokgithub@gmail.com>
2024-06-17 02:28:54 +0800

    Fix parallel processing

--- NAS/parallel_processing.py
+++ NAS/parallel_processing.py
@@ -1,5 +1,8 @@
 import multiprocessing
+import multiprocessing.queues
+from multiprocessing import Pool
 from joblib import Parallel, delayed
+import time
 
 def worker(queue, func, args, n_jobs):
     dataStore = [None] * len(args)
@@ -14,19 +17,35 @@ def worker(queue, func, args, n_jobs):
     parallel = Parallel(n_jobs=n_jobs, require='sharedmem')
     parallel(delayed(funcWrapper)(i, *args[i]) for i in range(len(args)))
 
+# def executeParallel(func, args, n_jobs, timeout):
+#     results = []
+#     for i in range(0, len(args), n_jobs):
+#         argsToUse = args[i:min(i+n_jobs, len(args))]
+
+#         queue = multiprocessing.Queue()
+#         p = multiprocessing.Process(target=worker, args=(queue, func, argsToUse, n_jobs))
+#         p.start()
+#         p.join(timeout=timeout)
+
+#         currentReults = queue.get_nowait()
+#         results+=currentReults
+#         if p.is_alive():
+#             p.terminate()
+#             p.join()
+#     return results
+
 def executeParallel(func, args, n_jobs, timeout):
     results = []
+
+    def callback(result):
+        results.append(result)
+    
     for i in range(0, len(args), n_jobs):
+        startTime = time.time()
+        pool = Pool(processes=n_jobs)
         argsToUse = args[i:min(i+n_jobs, len(args))]
-
-        queue = multiprocessing.Queue()
-        p = multiprocessing.Process(target=worker, args=(queue, func, argsToUse, n_jobs))
-        p.start()
-        p.join(timeout=timeout)
-
-        currentReults = queue.get_nowait()
-        results+=currentReults
-        if p.is_alive():
-            p.terminate()
-            p.join()
+        [pool.apply_async(func, args=eachArgs, callback=callback) for eachArgs in argsToUse]
+        time.sleep(timeout)
+        print(time.time() - startTime)
+        pool.terminate()
     return results
