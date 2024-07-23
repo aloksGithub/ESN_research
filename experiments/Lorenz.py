68363fb6d8e25c4ec8a2f8770122e2aa66aef9a0
Alok <alokgithub@gmail.com>
2024-07-24 01:07:30 +0800

    Setup lorenz experiment

--- experiments/Lorenz.py
+++ experiments/Lorenz.py
@@ -1,26 +1,57 @@
+import math
 import reservoirpy as rpy
 import numpy as np
 from functools import partial
 import sys
 import os
 import warnings
-import time
+current_dir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from NAS.ESN_NAS import ESN_NAS
+from NAS.utils import runModel
 current_dir = os.path.abspath(os.path.dirname(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 from NAS import NAS
 warnings.filterwarnings("ignore")
-import traceback
 import sys
 import pickle
-import matplotlib.pyplot as plt
+import pandas as pd
 
 rpy.verbosity(0)
+
+def nrmse(y_true, y_pred):
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    
+    rmse = np.sqrt(np.mean((y_true - y_pred)**2))
+    mean_norm = np.linalg.norm(np.mean(y_true))
+    error = rmse/mean_norm
+    if math.isnan(error):
+        return np.inf
+    else:
+        return error
+    
+def r_squared(y_true, y_pred):
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    numerator = np.sum((y_true - y_pred)**2)
+    denominator = np.sum((y_true - np.mean(y_true))**2)
+    r2 = 1 - (numerator / denominator)
+    if math.isnan(r2):
+        return 0
+    else:
+        return r2
+
+def readSavedExperiment(path):
+    file = open(path, 'rb')
+    return pickle.load(file)
 
 # https://www.sciencedirect.com/science/article/pii/S0925231222014291
 # Parameterizing echo state networks for multi-step time series prediction
 # Lorenz dataset
-
 def getData():
     data = np.load('./data/Lorenz_normed_2801.npy')
     from scipy import stats
@@ -38,52 +69,66 @@ def getData():
     test_out = data[trainLen+valLen+1:trainLen+valLen+testLen+1]
     return train_in, train_out, val_in, val_out, test_in, test_out
 
-trainX, trainY, valX, valY, testX, testY = getData()
+def printSavedResults():
+    nrmseErrors = []
+    r2_squaredValues = []
+    for i in range(5):
+        ga = readSavedExperiment('backup/lorenz/backup_{}.obj'.format(i))
+        nrmseErrors.append(ga.bestFitness[0])
+        r2_squaredValues.append(ga.bestFitness[1])
+    print("Errors:")
+    print(nrmseErrors)
+    print(r2_squaredValues)
+    print("Averaged errors:")
+    print("NRMSE: {} ({})".format(np.average(nrmseErrors), np.std(nrmseErrors)))
+    print("R2: {} ({})".format(np.average(r2_squaredValues), np.std(r2_squaredValues)))
 
-gaParams = {
-    "evaluator": partial(NAS.evaluateArchitecture2, trainX=trainX, trainY=trainY, valX=valX, valY=valY),
-    "generator": partial(NAS.generateRandomArchitecture, sampleX=trainX[:2000], sampleY=trainY[:2000], validThreshold=2, numVal=200),
-    "populationSize": 100,
-    "eliteSize": 1,
-    "stagnationReset": 5,
-    "generations": 50,
-    "minimizeFitness": True,
-    "logModels": False,
-    "seedModels": [],
-    "crossoverProbability": 0.7,
-    "mutationProbability": 0.2,
-    "earlyStop": 0,
-    "n_jobs": 25,
-    "saveModels": True,
-    "dataset": "lorenz"
-}
-
-if __name__ == "__main__":
-    # nrmseErrors = []
-    # r2Errors = []
-    # for i in range(2, 3):
-    #     error = False
-    #     gaParams["experimentIndex"] = i
-    #     while True:
-    #         try:
-    #             nrmse, r2 = NAS.runGA(gaParams, error)
-    #             r2Errors.append(r2)
-    #             nrmseErrors.append(nrmse)
-    #             break
-    #         except:
-    #             print(traceback.format_exc())
-    #             error = True
-    # print(np.array(nrmseErrors).mean(), np.array(nrmseErrors).std())
-    # print(np.array(r2Errors).mean(), np.array(r2Errors).std())
-    convergenceLines = []
+def printOldSavedResults():
+    nrmseErrors = []
+    r2_squaredValues = []
     total = 0
     r2_total = 0
     for i in range(5):
-        if i==2: continue
-        file = open('backup/{}/backup_{}.obj'.format(gaParams["dataset"], i), 'rb')
-        data = pickle.load(file)
-        fitnesses = data["allFitnesses"]
-        minFitnesses = []
+        data = readSavedExperiment('old_backup/lorenz/backup_{}.obj'.format(i))
         total+=min(data["allFitnesses"])
+        nrmseErrors.append(min(data["allFitnesses"]))
+        r2_squaredValues.append(data["fitnesses2"][data["allFitnesses"].index(min(data["allFitnesses"]))])
         r2_total+=data["fitnesses2"][data["allFitnesses"].index(min(data["allFitnesses"]))]
-    print(total/4, r2_total/4)
+    print("Errors:")
+    print(nrmseErrors)
+    print(r2_squaredValues)
+    print("Averaged errors:")
+    print("NRMSE: {} ({})".format(np.average(nrmseErrors), np.std(nrmseErrors)))
+    print("R2: {} ({})".format(np.average(r2_squaredValues), np.std(r2_squaredValues)))
+
+if __name__ == "__main__":
+    trainX, trainY, valX, valY, testX, testY = getData()
+    nrmseErrors = []
+    r2_squaredValues = []
+    for i in [0, 1, 2, 3, 4]:
+        ga = ESN_NAS(
+            trainX,
+            trainY,
+            valX,
+            valY,
+            50,
+            50,
+            trainY.shape[-1],
+            n_jobs=10,
+            errorMetrics=[nrmse, r_squared],
+            defaultErrors=[np.inf, 0],
+            timeout=180,
+            numEvals=3,
+            saveLocation='backup/lorenz/backup_{}.obj'.format(i),
+            memoryLimit=756,
+            isAutoRegressive=True
+        )
+        ga.run()
+        nrmseErrors.append(ga.bestFitness[0])
+        r2_squaredValues.append(ga.bestFitness[1])
+    print("Errors:")
+    print(nrmseErrors)
+    print(r2_squaredValues)
+    print("Averaged errors:")
+    print("NRMSE: {} ({})".format(np.average(nrmseErrors), np.std(nrmseErrors)))
+    print("R2: {} ({})".format(np.average(r2_squaredValues), np.std(r2_squaredValues)))
