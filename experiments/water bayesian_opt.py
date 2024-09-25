ac97a3a201e6b98c93a9909f7e3eb776ab14180f
Alok <alokgithub@gmail.com>
2024-09-25 19:41:14 +0800

    Change bayesian optimization version

--- experiments/water bayesian_opt.py
+++ experiments/water bayesian_opt.py
@@ -36,39 +36,28 @@ def printSavedBoResults():
 
 if __name__ == "__main__":
     trainX, trainY, valX, valY, testX, testY, _ = getDataWater()
-    nrmseErrors = []
-    rSquaredValues = []
-    for i in [1, 2, 3, 4]:
-        baseArchitecture = {'nodes': [{'type': 'Input', 'params': {'input_dim': 1}}, {'type': 'Reservoir', 'params': {'units': 1000, 'lr': 0.9, 'sr': 0.9, 'input_connectivity': 0.25, 'rc_connectivity': 0.25}}, {'type': 'Ridge', 'params': {'output_dim': 1, 'ridge': 8.0e-05}}], 'edges': [[0, 1], [1, 2]]}
-        
-        ga_bo = ESN_BO(
-            trainX,
-            trainY,
-            valX,
-            valY,
-            500,
-            2000,
-            trainY.shape[-1],
-            baseArchitecture,
-            3,
-            3,
-            [nrmse, r_squared],
-            [np.inf, 0],
-            True,
-            180,
-            "backup_bo/water/backup_{}.obj".format(i),
-            False
-        )
-        errors, model = ga_bo.run()
-        preds = runModel(model, testX)
-        nrmseError = nrmse(testY, preds)
-        r2Error = r_squared(testY, preds)
-        nrmseErrors.append(nrmseError)
-        rSquaredValues.append(r2Error)
-        print("Result:", nrmseError, r2Error)
-    print("Errors:")
-    print(nrmseErrors)
-    print(rSquaredValues)
-    print("Averaged errors:")
-    print("NRMSE: {} ({})".format(np.average(nrmseErrors), np.std(nrmseErrors)))
-    print("R2: {} ({})".format(np.average(rSquaredValues), np.std(rSquaredValues)))
+    baseArchitecture = {'nodes': [{'type': 'Input', 'params': {'input_dim': 1}}, {'type': 'Reservoir', 'params': {'units': 1000, 'lr': 0.9, 'sr': 0.9, 'input_connectivity': 0.25, 'rc_connectivity': 0.25}}, {'type': 'Ridge', 'params': {'output_dim': 1, 'ridge': 8.0e-05}}], 'edges': [[0, 1], [1, 2]]}
+    
+    ga_bo = ESN_BO(
+        trainX,
+        trainY,
+        valX,
+        valY,
+        500,
+        2000,
+        trainY.shape[-1],
+        baseArchitecture,
+        3,
+        3,
+        [nrmse, r_squared],
+        [np.inf, 0],
+        True,
+        180,
+        "backup_bo/water/backup_{}.obj".format(sys.argv[1]),
+        False
+    )
+    errors, model = ga_bo.run()
+    preds = runModel(model, testX)
+    nrmseError = nrmse(testY, preds)
+    r2Error = r_squared(testY, preds)
+    print("Result:", nrmseError, r2Error)
