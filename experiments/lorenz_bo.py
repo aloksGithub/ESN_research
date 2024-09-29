3bdf26ece98dcdae5afc633875933f1984929fb4
Alok <alokgithub@gmail.com>
2024-09-30 00:36:21 +0800

    Fix data shape error

--- experiments/lorenz_bo.py
+++ experiments/lorenz_bo.py
@@ -15,7 +15,7 @@ rpy.verbosity(0)
 if __name__ == "__main__":
     # printSavedBoResults("lorenz")
     trainX, trainY, valX, valY, testX, testY = getDataLorenz()
-    baseArchitecture = {'nodes': [{'type': 'Input', 'params': {'input_dim': 1}}, {'type': 'Reservoir', 'params': {'units': 1000, 'lr': 0.9, 'sr': 0.9, 'input_connectivity': 0.25, 'rc_connectivity': 0.25}}, {'type': 'Ridge', 'params': {'output_dim': 1, 'ridge': 8.0e-05}}], 'edges': [[0, 1], [1, 2]]}
+    baseArchitecture = {'nodes': [{'type': 'Input', 'params': {'input_dim': trainX.shape[-1]}}, {'type': 'Reservoir', 'params': {'units': 1000, 'lr': 0.9, 'sr': 0.9, 'input_connectivity': 0.25, 'rc_connectivity': 0.25}}, {'type': 'Ridge', 'params': {'output_dim': trainY.shape[-1], 'ridge': 8.0e-05}}], 'edges': [[0, 1], [1, 2]]}
         
     ga_bo = ESN_BO(
         trainX,
