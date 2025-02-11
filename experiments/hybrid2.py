5e0164badd770c911fb130923c61b0df4d099cd3
Alok <alokgithub@gmail.com>
2025-02-11 15:32:43 +0800

    Setup hybrid2

---
+++ experiments/hybrid2.py
@@ -0,0 +1,49 @@
+import reservoirpy as rpy
+import numpy as np
+import sys
+import os
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+from NAS.ESN_NAS2 import ESN_NAS2
+from NAS.error_metrics import nrmse, r_squared
+from utils import getDataMGS, printSavedResults
+import warnings
+warnings.filterwarnings("ignore")
+rpy.verbosity(0)
+
+if __name__ == "__main__":
+    trainX, trainY, valX, valY, testX, testY = getDataMGS()
+    nrmseErrors = []
+    r2_squaredValues = []
+    for i in [0, 1, 2, 3, 4]:
+        ga = ESN_NAS2(
+            trainX,
+            trainY,
+            valX,
+            valY,
+            5,
+            10,
+            trainY.shape[-1],
+            n_jobs=10,
+            errorMetrics=[nrmse, r_squared],
+            defaultErrors=[100000, 0],
+            timeout=60,
+            numEvals=3,
+            saveLocation='temp/mgs/backup_{}.obj'.format(i),
+            memoryLimit=756,
+            isAutoRegressive=True,
+            bo_init=5,
+            bo_iter=5
+        )
+        ga.run()
+        nrmseErrors.append(ga.bestFitness[0])
+        r2_squaredValues.append(ga.bestFitness[1])
+    print("Errors:")
+    print(nrmseErrors)
+    print(r2_squaredValues)
+    print("Averaged errors:")
+    print("NRMSE: {} ({})".format(np.average(nrmseErrors), np.std(nrmseErrors)))
+    print("R2: {} ({})".format(np.average(r2_squaredValues), np.std(r2_squaredValues)))
+
