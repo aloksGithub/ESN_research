3694789f10bc6d5c92b28e9242c127d52aeba64e
Alok <alokgithub@gmail.com>
2025-04-04 19:27:03 +0800

    Hybrid2 exploration

---
+++ experiments/hybrid2_exploration.py
@@ -0,0 +1,78 @@
+import reservoirpy as rpy
+import numpy as np
+import sys
+import os
+
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+from NAS.utils import runModel
+from NAS.ESN_NAS2 import ESN_NAS2
+from NAS.error_metrics import nrmse, nrmse_sunspots, r_squared
+from utils import getDataMGS, getDataDDE, getDataLaser, getDataLorenz, getDataSunspots, getDataWater, printSavedResults, printSavedResultsAutoRegressive
+import warnings
+warnings.filterwarnings("ignore")
+rpy.verbosity(0)
+
+def runExperiment(dataset, dataLoader, errorMetrics, isAutoregressive):
+    trainX, trainY, valX, valY, testX, testY = dataLoader()
+    nrmseErrors = []
+    r2_squaredValues = []
+    print(f'========================Starting GA for dataset {dataset}========================')
+    for i in [0]:
+        ga = ESN_NAS2(
+            trainX,
+            trainY,
+            valX,
+            valY,
+            2,
+            2,
+            trainY.shape[-1],
+            n_jobs=2,
+            errorMetrics=errorMetrics,
+            defaultErrors=[100000, 0],
+            timeout=60,
+            numEvals=3,
+            saveLocation='hybrid2_exploration/{}/backup_{}.obj'.format(dataset, i),
+            memoryLimit=756,
+            isAutoRegressive=isAutoregressive,
+            bo_init=1,
+            bo_iter=1
+        )
+        ga.run()
+        if isAutoregressive:
+            nrmseErrors.append(ga.bestFitness[0])
+            r2_squaredValues.append(ga.bestFitness[1])
+        else:
+            model = ga.bestModel
+            runModel(model, valX)
+            preds = runModel(model, testX)
+            nrmseError = nrmse(testY, preds)
+            r2Error = r_squared(testY, preds)
+            nrmseErrors.append(nrmseError)
+            r2_squaredValues.append(r2Error)
+    print(f'========================Performance for dataset {dataset}========================')
+    print("Errors:")
+    print(nrmseErrors)
+    print(r2_squaredValues)
+    print("Averaged errors:")
+    print("NRMSE: {} ({})".format(np.average(nrmseErrors), np.std(nrmseErrors)))
+    print("R2: {} ({})".format(np.average(r2_squaredValues), np.std(r2_squaredValues)))
+
+def printAllSavedResults():
+    printSavedResults('hybrid2_exploration', 'mgs')
+    printSavedResults('hybrid2_exploration', 'lorenz')
+    printSavedResults('hybrid2_exploration', 'dde')
+    printSavedResults('hybrid2_exploration', 'laser')
+    printSavedResultsAutoRegressive('hybrid2_exploration', 'sunspots', getDataSunspots)
+    printSavedResultsAutoRegressive('hybrid2_exploration', 'water', getDataWater)
+
+if __name__ == "__main__":
+    runExperiment('mgs', getDataMGS, [nrmse, r_squared], True)
+    runExperiment('lorenz', getDataLorenz, [nrmse, r_squared], True)
+    runExperiment('dde', getDataDDE, [nrmse, r_squared], True)
+    runExperiment('laser', getDataLaser, [nrmse, r_squared], True)
+    runExperiment('sunspots', getDataSunspots, [nrmse_sunspots, r_squared], False)
+    runExperiment('water', getDataWater, [nrmse, r_squared], False)
+
