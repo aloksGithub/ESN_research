5b796e5d3ec018a9b19236bd262b1601be10cd72
Alok <alokgithub@gmail.com>
2025-12-09 00:04:35 +0800

    Fix error functions

--- src/error_metrics.py
+++ src/error_metrics.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from reservoirpy.observables import (mse)
+from sklearn.metrics import r2_score
 
 def nrmse_sunspots(y_true, y_pred):
     mseError = mse(y_true, y_pred)
@@ -15,21 +16,20 @@ def nrmse(y_true, y_pred):
     y_true = np.array(y_true)
     y_pred = np.array(y_pred)
     
-    rmse = np.sqrt(np.mean((y_true - y_pred)**2))
-    mean_norm = np.linalg.norm(np.mean(y_true))
-    error = rmse/mean_norm
-    if math.isnan(error):
+    diff = y_true - y_pred
+    mean_sq_error = np.mean(np.sum(diff**2, axis=1))
+    mean_norm = np.linalg.norm(np.mean(y_true, axis=0))
+        
+    rmse = np.sqrt(mean_sq_error)
+    
+    if mean_norm == 0:
         return 100000
-    else:
-        return error
+        
+    error = rmse / mean_norm
+    return error
     
 def r_squared(y_true, y_pred):
-    y_true = np.array(y_true)
-    y_pred = np.array(y_pred)
-    numerator = np.sum((y_true - y_pred)**2)
-    denominator = np.sum((y_true - np.mean(y_true))**2)
-    r2 = 1 - (numerator / denominator)
-    if math.isnan(r2):
+    try:
+        return r2_score(y_true, y_pred, multioutput="variance_weighted")
+    except:
         return 0
-    else:
-        return r2
